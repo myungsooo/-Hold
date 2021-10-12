81c933ed777a0ff4a168297a7db5986820498320
soo <audtn0508@naver.com>
2021-10-12 23:19:57 +0900

    Player Code

--- Source/RollaBall/Game/RollaBallPlayer.cpp
+++ Source/RollaBall/Game/RollaBallPlayer.cpp
@@ -2,12 +2,27 @@
 
 
 #include "RollaBallPlayer.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 
 // Sets default values
 ARollaBallPlayer::ARollaBallPlayer()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
+
+	// Create Components - Before this step they won't exist on our Actor, they've only been defined.
+	Mesh = CreateDefaultSubobject<UStaticMeshComent>("Mesh");
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+
+	// Set the Root Component to be our Mesh.
+	RootComponent = Mesh;
+	// Attach the SpringArm to the Mesh, Spring will now follow the Mesh transform.
+	SpringArm->SetupAttachment(Mesh);
+	// Attach the Camera to the SpringArm, Camera will now follow the SpringArm transform.
+	Camera->SetupAttachment(SpringArm);
+
 
 }
 
@@ -18,17 +33,24 @@ void ARollaBallPlayer::BeginPlay()
 	
 }
 
-// Called every frame
-void ARollaBallPlayer::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
 
 // Called to bind functionality to input
 void ARollaBallPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+}
+
+
+void ARollaBallPlayer::MoveRight(float Value)
+{
+}
+
+void ARollaBallPlayer::MoveForward(float Value)
+{
+}
+
+void ARollaBallPlayer::Jump()
+{
 }
 
